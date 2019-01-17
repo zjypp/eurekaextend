cc4e4d87d4c0b8f736259ead6c7a951cee692cbe
jasonZhang <zjypp@126.com>
2019-01-17 10:01:39 +0800

    Initial commit

---
+++ eureka-server/src/main/java/com/zjy/eurekaserver/IndexController.java
@@ -0,0 +1,20 @@
+package com.zjy.eurekaserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+
+    @Autowired
+    EurekaClientConfigBean eurekaClientConfigBean;
+
+    @RequestMapping("/eureka-server")
+    public Object getEurekaServerUrl(){
+        return eurekaClientConfigBean.getServiceUrl();
+    }
+
+}
